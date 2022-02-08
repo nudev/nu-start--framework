10896ec4f56e785e4fec26870ea567e46e970ba2
tbielarski <t.bielarski@northeastern.edu>
2022-02-08 16:13:28 -0500

    Changes to People, Projects
    Added arrow to project, made changes to people

--- wp-content/themes/nu-start/classes/acf-blocks/posts-grid-item/templates/person.php
+++ wp-content/themes/nu-start/classes/acf-blocks/posts-grid-item/templates/person.php
@@ -14,20 +14,30 @@
 */
 
 
+// set the display fallback to post title
+$the_display_name = $the_post_title;
 
-// ? this is deprecated - will be replaced with single rich-text field soon (just notes)
-if( !empty( $fields['excerpt_title'] ) || !empty( $fields['excerpt_body'] ) ){
+// this fallback makes no damn sense but it works for now
+$the_primary_title = $the_basic_excerpt;
 
-  $guides['beta-person-excerpt'] = '<div class="person-excerpt">%1$s%2$s</div>';
+// Contact fallbacks
+$the_phone_number = ''; 
+$the_email_address = ''; 
 
-  $the_basic_excerpt = sprintf(
-    $guides['beta-person-excerpt'],
-    !empty($fields['excerpt_title']) ? '<p class="has-smaller-font-size"><i>'.$fields['excerpt_title'].'</i></p>' : '',
-    !empty($fields['excerpt_body']) ? '<p class="has-smaller-font-size">'.$fields['excerpt_body'].'</p>' : ''
-  );
+// if we have data
+if( !empty($person_metadata) ){
   
-}
+  // print_r($person_metadata);
+  // name, phone, email, primary title
+  
+  // ? get the display name or nothing
+  $the_display_name = !empty($person_metadata['display_name']) ? $person_metadata['display_name'] : $the_post_title;
+  
+  $the_phone_number = !empty($person_metadata['person_phone_number']) ? $person_metadata['person_phone_number'] : ''; 
 
+  $the_email_address = !empty($person_metadata['person_email']) ? $person_metadata['person_email'] : '';
+
+}
 
 
 // This template is loaded by the post grid
@@ -40,6 +50,8 @@ $guides['person'] = '
         %3$s
         <div class="is_the_rich_text_person_excerpt">
           %4$s
+          %9$s
+          %10$s
         </div>
       </div>
     </a>
@@ -48,14 +60,16 @@ $guides['person'] = '
 
 $return .= sprintf(
   $guides['person'],
-  ' '.$post_type,
-  $the_cover_image,
-  $the_post_title,
-  $the_basic_excerpt,
-  $determined_permalink,
-  $aspect_ratio_class,
-  $orientationClass,
-  $maybe_target,
+    ' '.$post_type,
+    $the_cover_image,
+    $the_display_name,
+    $the_primary_title,
+    $determined_permalink,
+    $aspect_ratio_class,
+    $orientationClass,
+    $maybe_target,
+    $the_phone_number,
+    $the_email_address
 );
 
 
