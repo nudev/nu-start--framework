5b482f7b2e1ea2b74318400d595766842dd1a6cd
kmarcy724 <k.marcy@northeastern.edu>
2022-02-09 13:03:29 -0500

    scaffolding separate stylesheets per block

--- wp-content/themes/nu-start/functions/gutenberg.php
+++ wp-content/themes/nu-start/functions/gutenberg.php
@@ -4,6 +4,42 @@
  */
 // 
 
+// Add frontend styles.
+add_action( 'wp_enqueue_scripts', 'nu__enqueue_each_block_styles' );
+// Add editor styles.
+add_action( 'admin_init', 'nu__enqueue_each_block_styles' );
+
+
+/**
+ * Attach extra styles to multiple blocks.
+ */
+function nu__enqueue_each_block_styles() {
+	// An array of blocks.
+	$styled_blocks = [ 'button' ];
+
+	foreach ( $styled_blocks as $block_name ) {
+		// Get the stylesheet handle. This is backwards-compatible and checks the
+		// availability of the `wp_should_load_separate_core_block_assets` function,
+		// and whether we want to load separate styles per-block or not.
+		$handle = (
+			function_exists( 'wp_should_load_separate_core_block_assets' ) &&
+			wp_should_load_separate_core_block_assets()
+		) ? "wp-block-$block_name" : 'wp-block-library';
+
+		// Get the styles.
+		$styles = file_get_contents( get_theme_file_path( "__precomp/build/css/blocks/$block_name.css" ) );
+
+		// Add frontend styles.
+		wp_add_inline_style( $handle, $styles );
+
+		// Add editor styles.
+		add_editor_style( "__precomp/build/css/blocks/$block_name.css" );
+		if ( file_exists( get_theme_file_path( "__precomp/build/css/blocks/$block_name-editor.css" ) ) ) {
+			add_editor_style( "__precomp/build/css/blocks/$block_name-editor.css" );
+		}
+	}
+}
+
 
 
 
