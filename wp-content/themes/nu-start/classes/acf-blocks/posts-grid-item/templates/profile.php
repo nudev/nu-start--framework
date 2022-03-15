b0f4ca269b40f26815ec5e5cf7d8aab2dfb05392
kmarcy724 <k.marcy@northeastern.edu>
2022-03-15 10:18:49 -0400

    touch ups

--- wp-content/themes/nu-start/classes/acf-blocks/posts-grid-item/templates/profile.php
+++ wp-content/themes/nu-start/classes/acf-blocks/posts-grid-item/templates/profile.php
@@ -6,21 +6,27 @@
  */
 // 
 
-$guides['profile'] = '
-<li class="grid-item%1$s%6$s%7$s">
-  <a class="contains-clickable-area" href="%5$s"'.$the_title_attribute.' %8$s>
-    %2$s
-    <div class="grid-item-content">
-      %3$s
-      %4$s
-      <div class="is-hanging-icon"><span class="material-icons-outlined">arrow_forward</span></div>
-    </div>
-  </a>
-</li>
+
+// 
+// 
+$guides['project'] = '
+  <li class="grid-item%1$s%6$s%7$s">
+    <a class="contains-clickable-area" href="%5$s"'.$the_title_attribute.' %8$s>
+      %2$s
+      <div class="grid-item-content">
+        %3$s
+        %4$s
+        <div class="is-hanging-icon"><span class="material-icons-outlined">arrow_forward</span></div>
+      </div>
+    </a>
+  </li>
 ';
 
+
+// 
+// 
 $return .= sprintf(
-  $guides['profile'],
+  $guides['project'],
   ' '.$post_type,
   $the_cover_image,
   $the_post_title,
